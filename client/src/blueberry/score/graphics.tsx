cd8eebce1e619f5520bd4d9e717b4b4dba8fe105
Hugo Hua <hkh4@williams.edu>
2022-01-19 23:09:51 -0500

    title

--- client/src/blueberry/score/graphics.tsx
+++ client/src/blueberry/score/graphics.tsx
@@ -1,25 +1,68 @@
-import { Page } from "./types"
+import { Page, PropertyList, OptionsRecord } from "./types"
 
 
 
-export function show(pages: Page[], tuningNumbers: number[]) {
+function showPage() {
 
-  const svgs = pages.map((page, index) => {
+}
 
-    const x = <svg key={index} height="100" width="100" viewBox="0 0 200 200">
-      <text x="50" y="50">One svg</text>
-    </svg>
 
-    return x
-  })
 
-  const result = <>
-    {svgs.map(s => {
-      return s
+/* Graphics driver
+1. pages
+2. optionsRecord: all the starting options info to be displayed
+*/
+export function show(pages: Page[], optionsR: OptionsRecord) {
+
+  const defaultPropertyList : PropertyList = {
+    slurStart: [[0.0,0.0], false, false],
+    muteStart: [[0.0,0.0], false],
+    tieStart: {
+      s1: [[0.0,0.0], 0.0, false, false],
+      s2: [[0.0,0.0], 0.0, false, false],
+      s3: [[0.0,0.0], 0.0, false, false],
+      s4: [[0.0,0.0], 0.0, false, false],
+      s5: [[0.0,0.0], 0.0, false, false],
+      s6: [[0.0,0.0], 0.0, false, false]
+    },
+    slideStart: {
+      s1: [[0.0,0.0], 0.0, false, false],
+      s2: [[0.0,0.0], 0.0, false, false],
+      s3: [[0.0,0.0], 0.0, false, false],
+      s4: [[0.0,0.0], 0.0, false, false],
+      s5: [[0.0,0.0], 0.0, false, false],
+      s6: [[0.0,0.0], 0.0, false, false]
+    },
+    slideStubs: {
+      s1: [[0.0,0.0], 0.0, false, false],
+      s2: [[0.0,0.0], 0.0, false, false],
+      s3: [[0.0,0.0], 0.0, false, false],
+      s4: [[0.0,0.0], 0.0, false, false],
+      s5: [[0.0,0.0], 0.0, false, false],
+      s6: [[0.0,0.0], 0.0, false, false]
+    },
+    hammerStart: {
+      s1: [[0.0,0.0], 0.0, false, false],
+      s2: [[0.0,0.0], 0.0, false, false],
+      s3: [[0.0,0.0], 0.0, false, false],
+      s4: [[0.0,0.0], 0.0, false, false],
+      s5: [[0.0,0.0], 0.0, false, false],
+      s6: [[0.0,0.0], 0.0, false, false]
+    }
+  }
+
+  const result = <svg viewBox="0 0 612 792">
+
+    <text x="306" y="47" textAnchor="middle" className="title">{optionsR.title}</text>
+
+    {pages.map((page, index) => {
+      return showPage()
     })}
-  </>
+
+  </svg>
 
   return result
+
 
 }
 
